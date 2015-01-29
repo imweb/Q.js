5df7bb590542833e810347d06af2e27735dfe1b5
Daniel Yang <miniflycn@justany.net>
2015-01-29 19:07:00 +0800

    update test cases

---
+++ test/utils.js
@@ -0,0 +1,58 @@
+var _ = require('utils');
+
+describe('utils', function () {
+    it('should find a element', function () {
+        var els = _.find('#container');
+        els.length.should.equal(1);
+        els[0].should.equal(document.getElementById('container'));
+    });
+
+    it('should able to copy a array use slice', function () {
+        var arr = [1, 2, 3],
+            copy = _.slice.call(arr, 0);
+        copy.should.not.equal(arr);
+        copy.should.eql(arr);
+    });
+
+    it('should able check contains', function () {
+        var container = _.find('#container')[0],
+            item = _.find('#item')[0];
+
+        _.contains(container, item).should.equal(true);
+    });
+
+    it('should able to set and get data', function () {
+        var container = _.find('#container')[0];
+        _.data(container, 'test', 'test');
+        _.data(container, 'test').should.equal('test');
+    });
+
+    it('should able to add & remove event', function (done) {
+        var container = _.find('#container')[0],
+            handle = function () {
+                _.remove(container, 'click', handle);
+                done();
+            };
+        _.add(container, 'click', handle);
+        container.click();
+    });
+
+    it('should able to extend objects', function () {
+        var target = _.extend({}, { test: 'test' });
+        target.extend = _.extend;
+        target.extend({
+            hello: 'hello'
+        });
+
+        target.should.have.property('test');
+        target.should.have.property('hello');
+    });
+
+    it('should able to add & remove class', function () {
+        var container = _.find('#container')[0];
+        _.addClass(container, 'test');
+        container.className.should.equal('test');
+        _.removeClass(container, 'test');
+        container.className.should.equal('');
+    });
+});
