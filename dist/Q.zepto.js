5d6e25c788061d1570ebdee61311942d22e8d25d
Daniel Yang <miniflycn@justany.net>
2015-01-31 22:57:39 +0800

    just use zepto data method

--- dist/Q.zepto.js
+++ dist/Q.zepto.js
@@ -541,23 +541,19 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(4),
-	    _extend = $.extend,
-	    _expando = 'QDataUid',
-	    _uid = 0,
-	    _map = {};
+	    _extend = $.extend;
 
 	module.exports = {
 	    find: $,
 	    contains: $.contains,
 	    data: function (el, key, value) {
-	        var uid = el[_expando] = el[_expando] || ++_uid,
-	            data = _map[uid] = _map[uid] || {};
-	        // set Data
-	        if (value === undefined) return data[key];
-	        return (data[key] = value);
+	        return $(el).data(key, value);
 	    },
-	    // TODO
-	    cleanData: function () {},
+	    cleanData: function (eles) {
+	        for (var i = 0, l = eles.length; i < l; i++) {
+	            $(eles[i]).removeData();
+	        }
+	    },
 	    add: $.event.add,
 	    remove: $.event.remove,
 	    clone: function (ele) {
