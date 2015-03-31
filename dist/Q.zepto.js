9ac1c9e1ea737f14cabc17e92d9d67815ecb6c44
Daniel Yang <miniflycn@justany.net>
2015-04-01 00:03:54 +0800

    clean up comment

--- dist/Q.zepto.js
+++ dist/Q.zepto.js
@@ -1303,57 +1303,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    }, typeof data[key] === 'object', options.immediate || (data[key] !== undefined));
 	                    if (_.isObject(directive) && directive.bind) directive.bind.call(that);
 	                });
-
-	            // // directive is repeat
-	            // name === 'repeat' &&
-	            //     // has parentNode, so this is not a template
-	            //     node.parentNode &&
-	            //     // just stop
-	            //     (setting.stop = true) &&
-	            //         descriptors.forEach(function (descriptor) {
-	            //             var key = descriptor.target,
-	            //                 target = namespace ? ([namespace, key].join('.')) : key,
-	            //                 readFilters = self._makeReadFilters(descriptor.filters),
-	            //                 repeats = [],
-	            //                 tpl = node,
-	            //                 ref = document.createComment('q-repeat'),
-	            //                 parentNode = node.parentNode;
-	            //             parentNode.replaceChild(ref, tpl);
-	            //             _walk([tpl], _.noop, {
-	            //                 useCache: true
-	            //             });
-	            //             readFilters.push(function (arr) {
-	            //                 if (repeats.length) {
-	            //                     repeats.forEach(function (node) {
-	            //                         // repeat element may has been remove
-	            //                         node.parentNode === parentNode &&
-	            //                             parentNode.removeChild(node);
-	            //                     });
-	            //                     _.cleanData(repeats);
-	            //                     repeats.length = 0;
-	            //                 }
-	            //                 var fragment = document.createDocumentFragment(),
-	            //                     itemNode;
-	            //                 arr.forEach(function (obj, i) {
-	            //                     itemNode = _.clone(tpl);
-	            //                     self._templateBind(itemNode, {
-	            //                         data: obj,
-	            //                         namespace: obj.$namespace(),
-	            //                         immediate: true,
-	            //                         useCache: true
-	            //                     });
-	            //                     repeats.push(itemNode);
-	            //                     fragment.appendChild(itemNode);
-	            //                 });
-	            //                 parentNode.insertBefore(fragment, ref);
-	            //             });
-	            //             self.$watch(target, function (value) {
-	            //                 _.nextTick(function () {
-	            //                     self.applyFilters(value, readFilters);
-	            //                     self.$emit('repeat-render');
-	            //                 });
-	            //             }, false, true);
-	            //         });
 	        });
 	    }, {
 	        useCache: options.useCache
